1dac2a0f06931f998841fe8b7ca7f85ed81f1931
Abdo-salem9982 <68501304+Abdo-salem9982@users.noreply.github.com>
2022-04-26 01:01:43 +0200

    test

--- 06_IteratorPattern/IteratorPattern_in_JavaScrept.js
+++
@@ -1,9 +0,0 @@
-let evenNumbers=['1','2','3','4','5','6']
-let iterator = evenNumbers[Symbol.iterator]();
-
-let result = iterator.next();
-
-while( !result.done ) {
-    console.log(result.value);
-    result = iterator.next();
-}
